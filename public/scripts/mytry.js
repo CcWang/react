00dd117dd595ef023322c421814e18e66f46ef6f
Chongchong Wang <chongchong1110@gmail.com>
2016-08-08 23:43:29 -0700

    hello world

---
+++ public/scripts/mytry.js
@@ -0,0 +1,22 @@
+/***
+- Comment BOX
+	-CommentList
+		-Comment
+	-CommentForm
+***/
+
+var CommentBox=React.createClass({
+	render: function () {
+		return (
+			// classname begin with an lower case
+			<div className='commentBox'>
+				<h1>Hello, This is a commnet box</h1>
+			</div>
+		);
+	}
+});
+ReactDOM.render(
+	// begin with an uppercase letter
+	<CommentBox />,
+	document.getElementById('content')
+);

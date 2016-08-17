06e0bf4482fce1a86b74cc0a9ea4d7306327cc30
CC Wang <chongchong1110@gmail.com>
2016-08-17 16:50:42 -0700

    finish mark completed function

--- public/scripts/todoList.js
+++ public/scripts/todoList.js
@@ -16,12 +16,27 @@ var WholeList = React.createClass({
       console.log(this.state.tasksList)
     }.bind(this));
   },
+  handleComplete:function(taskName){
+    console.log(taskName)
+    var tasksListNew=this.state.tasksList
+    tasksListNew.forEach(function(p){
+      if (p.name ===taskName){
+        p.completed = !(p.completed)
+      }
+    });
+    this.setState({
+      tasksList:tasksListNew
+    })
+  },
   render:function(){
     return (
       <div>
       <h1> This is TO DO List </h1>
       <SearchBar tasksList={this.state.tasksList}/>
-      <TodoList tasksList={this.state.tasksList}/>
+      <TodoList
+        tasksList={this.state.tasksList}
+        handleComplete={this.handleComplete}
+      />
       <AddTask
         tasksList={this.state.tasksList} handleAddTask={this.handleAddTask}
       />
@@ -53,11 +68,10 @@ var TodoList=React.createClass({
 
     if (this.props.tasksList){
       this.props.tasksList.forEach(function(t){
-        console.log(t.name)
         if (t.completed){
-          rowC.push(<TaskC task={t} key={t.name}/>)
+          rowC.push(<TaskC task={t} key={t.name} handleComplete={this.props.handleComplete}/>)
         }else{
-          rowU.push(<Task task={t} key={t.name}/>)
+          rowU.push(<Task task={t} key={t.name} handleComplete={this.props.handleComplete}/>)
 
         }
       }.bind(this));
@@ -76,17 +90,19 @@ var Task = React.createClass({
   handleRemove:function(){
     console.log('remove')
   },
+  handleComplete:function(){
+    this.props.handleComplete(this.props.task.name);
+  },
   render:function () {
     return(
       <tr>
         <td>
           <span
             className='glyphicon glyphicon-ok'
-            style={{color:'grey'}}>
+            onClick={this.handleComplete}>
           </span> {this.props.task.name}
           <span
             className='glyphicon glyphicon-remove'
-            style={{color:'grey'}}
             onClick={this.handleRemove}>
 
           </span>
@@ -99,17 +115,18 @@ var TaskC = React.createClass({
   handleRemove:function(){
     console.log('remove')
   },
+  handleComplete:function(){
+    this.props.handleComplete(this.props.task.name);
+  },
   render:function () {
     return(
-      <tr style={{'background-color':'lightblue'}}>
-        <td className="completed">
+      <tr style={{'backgroundColor':'lightblue'}}>
+        <td className="completed" onClick={this.handleComplete}>
           <span
-            className='glyphicon glyphicon-ok'
-            style={{color:'grey'}}>
+            className='glyphicon glyphicon-ok'>
           </span> {this.props.task.name}
           <span
             className='glyphicon glyphicon-remove'
-            style={{color:'grey'}}
             onClick={this.handleRemove}>
 
           </span>
